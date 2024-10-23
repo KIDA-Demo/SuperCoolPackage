e9f7b28cf6e88c0951b8e791eca821dc90b64351
Steffen Albrecht <steffen.alb@posteo.de>
2024-10-23 09:55:58 +0200

    adding some python code

---
+++ main.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+import argparse
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser("===Super Cool Application===")
+  parser.add_argument("name", help="name of a person")
+
+args = parser.parse_args()
+if args.name == None:
+  print("hello world!")
+else:
+  print("hello {}".format(args.name))
